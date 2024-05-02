9fbbb6d916b987fa116741b90e65d78c44337031
mia <miayu.1008@gmail.com>
2024-05-02 16:37:20 +0900

    edited login and register screens

--- app/src/main/java/com/example/mia_hometest/LoginFragment.java
+++ app/src/main/java/com/example/mia_hometest/LoginFragment.java
@@ -9,9 +9,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class LoginFragment extends Fragment {
     private static final String TAG = LoginFragment.class.getSimpleName();
@@ -19,6 +26,9 @@ public class LoginFragment extends Fragment {
     private Context mContext = null;
     private TextView mRegister = null;
     private TextView mLogin = null;
+    private EditText mEmail = null;
+    private EditText mPwd = null;
+    private ImageView mPiggy = null;
     private RegisterFragment mRegisFragment = null;
 
     public LoginFragment (Context context) {
@@ -41,18 +51,36 @@ public class LoginFragment extends Fragment {
         mRegisFragment = new RegisterFragment(mContext);
         mSharedPreference = mContext.getSharedPreferences("check_login", Context.MODE_PRIVATE);
 
-        mLogin = view.findViewById(R.id.save_title);
-        mRegister = view.findViewById(R.id.regis_title);
+        mLogin = view.findViewById(R.id.loginBtn);
+        mRegister = view.findViewById(R.id.registerBtn);
+        mEmail = view.findViewById(R.id.set_email);
+        mPwd = view.findViewById(R.id.set_pwd);
+        mPiggy = view.findViewById(R.id.piggy);
+        GlideDrawableImageViewTarget gif = new GlideDrawableImageViewTarget(mPiggy); //gif setting
+        Glide.with(mContext).load(R.drawable.piggy_money).into(gif);
 
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "onClick: 클릭했다 로그인하자");
-                SharedPreferences.Editor editor = mSharedPreference.edit();
-                editor.putBoolean("login", true);
-                editor.apply();
-                Intent intent = new Intent(mContext, UserMainActivity.class);
-                startActivity(intent);
+                String email = mEmail.getText().toString();
+                String pwd = mPwd.getText().toString();
+
+                if (email.isEmpty()) {
+                    Animation shake = AnimationUtils.loadAnimation(mContext, R.anim.shake);
+                    mEmail.startAnimation(shake);
+                }
+                else if (pwd.isEmpty()) {
+                    Animation shake = AnimationUtils.loadAnimation(mContext, R.anim.shake);
+                    mPwd.startAnimation(shake);
+                }
+                else {
+                    Log.d(TAG, "onClick: 클릭했다 로그인하자");
+                    SharedPreferences.Editor editor = mSharedPreference.edit();
+                    editor.putBoolean("login", true);
+                    editor.apply();
+                    Intent intent = new Intent(mContext, UserMainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
--- app/src/main/java/com/example/mia_hometest/RegisterFragment.java
+++ app/src/main/java/com/example/mia_hometest/RegisterFragment.java
@@ -5,14 +5,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -21,11 +29,13 @@ import java.util.Map;
 public class RegisterFragment extends Fragment {
     private final String TAG = RegisterFragment.class.getSimpleName();
     private SharedPreferences mSharedPreference;
+    private static final Boolean mValue = true;
     private Context mContext = null;
     private TextView mCancel = null;
     private TextView mSave = null;
     private EditText mEmail = null;
-    private EditText mName = null;
+    private EditText mPwd = null;
+    private ImageView mMoneyImg = null;
 
     public RegisterFragment (Context context) {
         mContext = context;
@@ -44,20 +54,37 @@ public class RegisterFragment extends Fragment {
         View view = inflater.inflate(R.layout.register, container, false);
 
         mSharedPreference = mContext.getSharedPreferences("check_login", Context.MODE_PRIVATE);
-        mSave = view.findViewById(R.id.save_title);
-        mCancel = view.findViewById(R.id.cancel);
+        mSave = view.findViewById(R.id.signBtn);
+        mCancel = view.findViewById(R.id.cancelBtn);
         mEmail = view.findViewById(R.id.set_email);
-        mName = view.findViewById(R.id.set_name);
+        mPwd = view.findViewById(R.id.set_pwd);
+        mMoneyImg = view.findViewById(R.id.moneyImg);
+        GlideDrawableImageViewTarget gif = new GlideDrawableImageViewTarget(mMoneyImg); //gif setting
+        Glide.with(mContext).load(R.drawable.money_stack).into(gif);
 
         mSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addNewData();
-                SharedPreferences.Editor editor = mSharedPreference.edit();
-                editor.putBoolean("login", true);
-                editor.apply();
-                Intent intent = new Intent(mContext, UserMainActivity.class);
-                startActivity(intent);
+                String email = mEmail.getText().toString();
+                String pwd = mPwd.getText().toString();
+
+                if (email.isEmpty()) {
+                    Log.d(TAG, " ##### you cannot login without typing yout id on textbox ");
+                    Animation shake = AnimationUtils.loadAnimation(mContext, R.anim.shake);
+                    mEmail.startAnimation(shake);
+                }
+                else if (pwd.isEmpty()) {
+                    Animation shake = AnimationUtils.loadAnimation(mContext, R.anim.shake);
+                    mPwd.startAnimation(shake);
+                }
+                else {
+                    addNewData();
+                    SharedPreferences.Editor editor = mSharedPreference.edit();
+                    editor.putBoolean("login", true);
+                    editor.apply();
+                    Intent intent = new Intent(mContext, UserMainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -71,11 +98,12 @@ public class RegisterFragment extends Fragment {
     }
 
     private void addNewData() {
+        Log.d(TAG, " ##### put new data resource into the db");
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         Map<String, Object> user = new HashMap<>();
         user.put("email", mEmail.getText().toString());
-        user.put("name", mName.getText().toString());
+        user.put("name", mPwd.getText().toString());
         
         db.collection("user")
                 .add(user)
