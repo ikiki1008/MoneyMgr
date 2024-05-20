cccac1208cc0a3ea4b3bb6c700129a28a854e88b
mia <miayu.1008@gmail.com>
2024-05-21 00:39:34 +0900

    changed icons

--- app/src/main/java/com/example/mia_hometest/fragments/informations/UserInfoFragment.java
+++ app/src/main/java/com/example/mia_hometest/fragments/informations/UserInfoFragment.java
@@ -301,7 +301,7 @@ public class UserInfoFragment extends Fragment implements View.OnClickListener{
                 Log.d(TAG, "onClick: 이미지 클릭했다");
                 if (mEditTexts.get(2).getTransformationMethod() == PasswordTransformationMethod.getInstance()) {
                     mEditTexts.get(2).setTransformationMethod(null);
-                    mLock.setImageResource(R.drawable.unlocked);
+                    mLock.setImageResource(R.drawable.unlock);
                     Log.d(TAG, "onClick: 이미지 잠금 해제");
                 } else {
                     mEditTexts.get(2).setTransformationMethod(PasswordTransformationMethod.getInstance());
